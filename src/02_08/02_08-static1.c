2062fca172070253a52949eb5377b0e75e2dd8d4
datttrian <datttrian@gmail.com>
2023-09-07 12:47:30 +0000

    Making new data types

---
+++ src/02_08/02_08-static1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char *myname(void)
+{
+	static char me[] = "Dan Gookin";
+
+	return (me);
+}
+
+int main()
+{
+	printf("This course's author is %s\n", myname());
+
+	return (0);
+}
