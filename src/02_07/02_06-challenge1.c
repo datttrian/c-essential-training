6f97b3017288716ec2c783876a2b2a8150487470
datttrian <datttrian@gmail.com>
2023-09-07 12:36:28 +0000

    Solution: Make variables and constants

---
+++ src/02_07/02_06-challenge1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	const char b = 'Z';
+
+	a = 17;
+	printf("Here is the value of A: %d\n", a);
+	printf("Here is the value of B: %c\n", b);
+
+	return (0);
+}
